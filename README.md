72295fd2a4b61762f1766207b7fb8c8c6cb06d3e
Niels Holt <niels@steenbuchholt.dk>
2020-09-24 16:38:21 +0200

    No message

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # fcoo-promise-list
-FCOO version of promise-list
+
+
+
+## Description
+Create a version of promise-list as `window.fcoo.promiseList`
+
+## Copyright and License
+This plugin is licensed under the [MIT license](https://github.com/FCOO/fcoo-promise-list/LICENSE).
+
+Copyright (c) 2020 [FCOO](https://github.com/FCOO)
+
+## Contact information
+
+Niels Holt nho@fcoo.dk
---
+++ src/fcoo-promise-list.js
@@ -0,0 +1,21 @@
+/****************************************************************************
+    fcoo-promise-list.js,
+
+    (c) 2020, FCOO
+
+    https://github.com/FCOO/fcoo-promise-list
+    https://github.com/FCOO
+
+****************************************************************************/
+
+(function (window/*, document, undefined*/) {
+    "use strict";
+
+    //Create fcoo-namespace
+    var ns = window.fcoo = window.fcoo || {};
+
+    ns.promiseList = new window.PromiseList({
+
+    });
+
+}(this, document));
